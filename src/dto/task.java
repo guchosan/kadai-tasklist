f73085976ed1b74d2b918c30949c5b87a1f86af1
osamu <osamu1901@gmail.com>
2018-08-26 13:34:28 +0900

    Add database files

---
+++ src/dto/task.java
@@ -0,0 +1,37 @@
+package dto;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+
+@Entity
+@NamedQueries({
+    @NamedQuery(
+        name = "getAlltasks",
+        query = "SELECT m FROM task AS m ORDER BY m.id DESC"
+        )
+})
+
+@Table(name = "tasks")
+public class task {
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    @Column(name = "content", length = 255, nullable = false)
+    private String content;
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+}
